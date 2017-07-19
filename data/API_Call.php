c0e6bf09d1a3d70a03833c04147462460774e14f
eenblam <ben.a.elam@gmail.com>
2017-07-18 19:43:07 -0500

    Add constructor & destructor to CallAPI

--- data/API_Call.php
+++ data/API_Call.php
@@ -1,13 +1,22 @@
 <?php
 class CallAPI
 {
-	public function loadSecrets() {
-		$auth_data = file_get_contents("secrets.json", true);
-		return json_decode($auth_data);
+	private $API_Key;
+	private $ch;
+
+	function __construct($secret_location) {
+		$secrets = file_get_contents($secret_location, true);
+		$secretsJSON = json_decode($secrets);
+		$this->API_Key = $secretsJSON->{'moviedb-api-key'};
+		$this->ch = curl_init();
+	}
+
+	function __destruct() {
+		curl_close($this->ch);
 	}
 
 	public function makeRequest($query) {
-		$ch = curl_init();
+		$ch = $this->ch;
 		curl_setopt($ch, CURLOPT_URL, $query);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 
@@ -15,19 +24,15 @@ class CallAPI
 		$output = curl_exec($ch);
 		//TODO Handle bad status
 		$json_output = json_decode($output);
-		curl_close($ch);
 		return $json_output;
 	}
 		
 	public function getPageIds($search) {
-		$auth = self::loadSecrets();
-		$API_Key = $auth->{'moviedb-api-key'};
-
 		//TODO Search to sanitized query string
 		$sanitized_query = $search;
 
-		$query = "https://api.themoviedb.org/3/search/keyword?query={$sanitized_query}&api_key={$API_Key}";
-		$output = self::makeRequest($query);
+		$query = "https://api.themoviedb.org/3/search/keyword?query={$sanitized_query}&api_key={$this->API_Key}";
+		$output = $this->makeRequest($query);
 		$results = $output->results;
 
 		return array_map(function ($x) { return $x->id; }, $results);
