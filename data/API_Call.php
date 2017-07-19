b17bf8e2930bad281b84204059008af51b3be034
eenblam <ben.a.elam@gmail.com>
2017-07-18 19:16:07 -0500

    Replace function with class of several methods

--- data/API_Call.php
+++ data/API_Call.php
@@ -1,28 +1,39 @@
 <?php
-function call_API($search) {
-        $auth_data = file_get_contents("secrets.json", true);
-	$auth = json_decode($auth_data);
-	$API_Key = $auth->{'moviedb-api-key'};
-
-	//TODO Search to sanitized query string
-	$sanitized_query = $search;
-
-	$query = "https://api.themoviedb.org/3/search/keyword?query={$sanitized_query}&api_key={$API_Key}";
-
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $query);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-
-	//TODO Is there an error to catch from curl_exec?
-        $output = curl_exec($ch);
-	//TODO Handle bad status
-        $output = json_decode($output);
-	$results = $output->results;
-
-	foreach ($results as $result)
-	{
-		echo $result->id, " ", $result->name, PHP_EOL;
+class CallAPI
+{
+	public function loadSecrets() {
+		$auth_data = file_get_contents("secrets.json", true);
+		return json_decode($auth_data);
 	}
-        curl_close($ch);
+
+	public function makeRequest($query) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $query);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+		//TODO Is there an error to catch from curl_exec?
+		$output = curl_exec($ch);
+		//TODO Handle bad status
+		$json_output = json_decode($output);
+		curl_close($ch);
+		return $json_output;
+	}
+		
+	public function getPageIds($search) {
+		$auth = self::loadSecrets();
+		$API_Key = $auth->{'moviedb-api-key'};
+
+		//TODO Search to sanitized query string
+		$sanitized_query = $search;
+
+		$query = "https://api.themoviedb.org/3/search/keyword?query={$sanitized_query}&api_key={$API_Key}";
+		$output = self::makeRequest($query);
+		$results = $output->results;
+
+		foreach ($results as $result)
+		{
+			echo $result->id, " ", $result->name, PHP_EOL;
+		}
+	}
 }
 ?>
