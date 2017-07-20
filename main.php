e5abfa6fe161d38f1bc9c55365fc951ad040fb08
eenblam <ben.a.elam@gmail.com>
2017-07-19 23:23:05 -0500

    Proof off concept for search

--- main.php
+++ main.php
@@ -1,3 +1,23 @@
 <?php
-echo "Hello World!";
-?> 
+include("resources/library/APICall.php");
+include("resources/library/MCAPI.php");
+
+$search = "groundhog";
+$api = new APICall("secrets.json");
+$mc = new MCAPI($api, $search, 3);
+$num_results = rand(0, 10);
+$movies = $mc->generateMovies($num_results);
+
+if (count(movies) > 0) {
+	echo '<ol>';
+	foreach ($movies as $movie) {
+		echo '<li>';
+		echo '<span class="title">' . $movie->title . '</span>';
+		echo '<span class="overview">' . $movie->overview . '</span>';
+		echo '</li>';
+	}
+	echo '</ol>';
+} else {
+	echo '<span id="movie-error">Could not find any movies for ' . $search . '.</span>';
+}
+?>
